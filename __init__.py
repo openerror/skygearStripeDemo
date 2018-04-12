8b7f8cc3750ce66841e7013a6f49420b6eb5b570
Everest Law <everestl@usc.edu>
2018-04-11 21:36:21 -0700

    Error handling for stripe added

--- __init__.py
+++ __init__.py
@@ -11,22 +11,74 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .util.skymail import Mailer
 import requests
 import skygear
 import stripe
-from . import settings
+from settings import stripe_settings
 
-# custom logic to be invoked from SDK, e.g.
-# skygear.lambda('food:buy', {'food': 'salmon'}) for JS
+stripe.api_key = stripe_settings["api_key"]
+
+# Function responsible for charging thge customer through Stripe
+# Invoked from HTML frontend via index.js
 @skygear.op('submitPayment', user_required=False)
 def submitPayment(stripeToken, charge):
     if stripeToken and charge:
-        return {
-            "success": True
-        }
-    else:
+        try:
+          # Use Stripe's library to make requests...
+          return {
+              "success": True,
+              "charge_amount": charge
+          }
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            return parseError(e)
+
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            return parseError(e)
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            return parseError(e)
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            return parseError(e)
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            return parseError(e)
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+
+            return parseError(e)
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            return parseError(e)
+
+    else: # Something is wrong with the arguments supplied
         return {
             "success": False,
             "error_msg": "Cloud::submitPayment(): Missing token or charge amount!"
         }
+
+def parseError(errorObj):
+    ''' For now, return all possible error content '''
+
+    body = errorObj.json_body
+    err  = body.get('error', {})
+    error_msg = {
+        "success": False,
+        "stripe_http_status": err.http_status,
+        "stripe_error_type": err.get('type'),
+        "stripe_error_charge": err.get('charge'),
+        "stripe_error_code": err.get('code'),
+        "stripe_error_decline_code": err.get('decline_code'),
+        "stripe_error_doc_url": err.get('doc_url'),
+        "stripe_error_message": err.get('message'),
+        "stripe_error_param": err.et('param')
+    }
+    return error_msg
--- settings.py
+++ settings.py
@@ -1,14 +1,3 @@
-config = {
-    "default_sender": "",
-    "default_reply_to": "",
-    "enable_grecaptcha": False,
-    "grecaptcha_secret": 'IGNORE THIS FIELD'
-}
-
-smtp_settings = {
-    "smtp_host":'smtp.gmail.com',
-    "smtp_port":587,
-    "smtp_mode":'tls',
-    "smtp_login":'',
-    "smtp_password":""
+stripe_settings = {
+    "api_key": "sk_test_P6MHOTpoPtoGM7yVarD5KJKi"
 }
